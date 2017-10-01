3b6449c5204361cfd582fe8f2bb6e5837e34b825
Chris Johnson <crjjrc@gmail.com>
2017-10-01 07:47:52 -0500

    Add autosize no-ID warning, fix autosize.

--- skins/flat/utilities.js
+++ skins/flat/utilities.js
@@ -1,7 +1,7 @@
 function autosize(id) {
-  var element = document.getElementById(id);
-  element.width = element.contentWindow.document.body.clientWidth;
-  element.height = element.contentWindow.document.body.clientHeight;
+  id = '#' + id;
+  $(id).height($(id).contents().outerHeight());
+  $(id).width($(id).contents().outerWidth());
 }
 
 function lineveil() {
