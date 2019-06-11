92eec8dbd5410676d47f6e745b88ed35eefa6ab2
Chris Johnson <crjjrc@gmail.com>
2019-06-12 08:01:11 +1200

    Allow Deltaphone embeds.

--- graytext.rb
+++ graytext.rb
@@ -484,6 +484,13 @@ deltaphones.forEach(function(id) {
   document.getElementById('deltaphone-form-' + id).submit();
 });
 
+$('.expandable-link').each(function(i, element) {
+  var formId = element.dataset.form;
+  element.onclick = function() {
+    document.getElementById(formId).submit();
+  };
+});
+
 </script>
 EOF
       end
@@ -1402,9 +1409,11 @@ EOF
               @i += 1
             end
 
+            is_expandable = !attributes.has_key?('expandable') || attributes['expandable'] != 'false'
+
             if @target == 'wordpress'
               dst += <<EOF
-[deltaphone id=#{attributes['id']} width=#{attributes['width']} height=#{attributes['height']}]#{code}[/deltaphone]</pre>
+[deltaphone id=#{attributes['id']} width=#{attributes['width']} height=#{attributes['height']} expandable=#{is_expandable}]#{code}[/deltaphone]</pre>
 EOF
             else
               code.gsub!(/</, '&lt;')
@@ -1412,14 +1421,19 @@ EOF
               @deltaphones << attributes['id']
               dst += <<EOF
 <form style="display: none" id="deltaphone-form-#{attributes['id']}" target="deltaphone-frame-#{attributes['id']}" action="#{@deltaphoneurl}" method="post">
-<textarea name="src">#{code}</textarea>
-EOF
-
-              dst += <<EOF
-<input type="submit"/>
+  <textarea name="src">#{code}</textarea>
+  <input type="submit"/>
 </form>
 <iframe id="deltaphone-frame-#{attributes['id']}" name="deltaphone-frame-#{attributes['id']}" src="" width="#{attributes['width']}" height="#{attributes['height']}" class="deltaphone-frame#{attributes.has_key?('class') ? " #{attributes['class']}" : ''}"></iframe>
 EOF
+              if is_expandable
+                dst += <<EOF
+<form style="display: none" id="deltaphone-expandable-form-#{attributes['id']}" action="#{@deltaphoneurl}" method="post" target="_blank">
+  <textarea name="src">#{code}</textarea>
+</form>
+<button class="expandable-link" data-form="deltaphone-expandable-form-#{attributes['id']}">expand</button>
+EOF
+              end
             end
 
           elsif command == 'twoville'
